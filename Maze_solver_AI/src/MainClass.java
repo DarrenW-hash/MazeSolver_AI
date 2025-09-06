faeac41f760d541c051bf6beda0d1a3609b77fcc
DarrenW-hash <darren1111weng@gmail.com>
2025-09-05 23:51:56 -0400

    3rd

--- Maze_solver_AI/src/MainClass.java
+++ Maze_solver_AI/src/MainClass.java
@@ -3,21 +3,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.awt.image.BufferedImage;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.io.PrintWriter;
 
 public class MainClass {
 	
 	public static char[][] ConvertImagetoChar(File imagefile) throws IOException{
 		BufferedImage image = ImageIO.read(imagefile);
-		int width = image.getWidth();
-		int height = image.getHeight();
+		BufferedImage ToScaleImage = reSizeImage(image);
+		
+		int width = ToScaleImage.getWidth();
+		int height = ToScaleImage.getHeight();
 		
 		char[][] mazeArray = new char[height][width];
 		
 		
 		for (int y = 0; y < height; y++) {
             for (int x = 0; x < width; x++) {
-                int pixel = image.getRGB(x, y);
+                int pixel = ToScaleImage.getRGB(x, y);
                 // Simple binarization: if close to white, it's a path, else a wall
                 if ((pixel & 0xFFFFFF) > 0x808080) { // Check if pixel is relatively light
                     mazeArray[y][x] = ' '; // Path
@@ -39,9 +43,27 @@ public class MainClass {
 		writer.close();
 	}
 	
+		public static BufferedImage reSizeImage(BufferedImage imageFile){
+			int newheight = 80;
+			int newwidth = 80;
+			
+			BufferedImage resizedImage = new BufferedImage(newwidth, newheight, imageFile.getType());
+			
+			Graphics2D g2d = resizedImage.createGraphics();
+			g2d.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+	                RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+			
+			g2d.drawImage(imageFile,0,0,newwidth, newheight, null);
+			g2d.dispose();
+			
+			return resizedImage;
+			//File mazefile = new File("/Users/darrenweng/git/MazeSolver_AI/Maze_solver_AI/src/maze-line-black-icon-png_309317.jpg");	
+		}
+	
+	
 	public static void main(String args []) throws IOException {
 		File mazefile = new File("/Users/darrenweng/git/MazeSolver_AI/Maze_solver_AI/src/maze-line-black-icon-png_309317.jpg");	
-		
+		//reSizeImage();
 		
 		char[][] maze = ConvertImagetoChar(mazefile);
 		sendtoOutputFile(maze);
